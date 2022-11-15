012aedb49156a7de41b8e247315d46fcf39ac72c
murgraydj <murgraydj@gmail.com>
2022-11-15 11:23:38 -0500

    Prints current board

--- board.rb
+++ board.rb
@@ -1,3 +1,23 @@
+# X | O | X
+# ----------
+# O | X | O
+# ----------
+# X | O | X
+
+
 class Board
 
+    def initialize
+        @board = [[nil,nil,nil],
+                [nil,nil,nil],
+                [nil,nil,nil]]
+    end
+
+    def print_board
+        p @board
+    end
+
 end
+
+board = Board.new
+board.print_board
