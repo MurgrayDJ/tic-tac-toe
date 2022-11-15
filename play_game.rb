60dbed007901876915a17710cc3ad2ecc5e75f3c
murgraydj <murgraydj@gmail.com>
2022-11-15 13:52:30 -0500

    Adds general input verification method
    Right now only the user inputed letter choice is validated.

--- play_game.rb
+++ play_game.rb
@@ -10,10 +10,26 @@ class PlayGame
 
     def createPlayer
         print "Enter name: "
-        name = gets
-        print "Enter letter: "
-        letter = gets
+        name = gets.chomp
+
+        letter = get_valid_data("Enter a letter: ", nil, ("A".."z")).upcase
+
         player = Player.new(name, letter)
+    end
+
+    def get_valid_data(prompt, response, valid_responses)
+        if response.nil?
+            print prompt
+            response = gets.chomp
+        else
+            valid_responses.each do |valid_response|
+                if response == valid_response
+                    return response
+                end
+            end
+            response = nil
+        end
+        response = get_valid_data(prompt, response, valid_responses)  
     end
 end
 
