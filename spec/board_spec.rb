2d03a575e585c2fa83516c361d7a4239f82d03a2
murgraydj <murgraydj@gmail.com>
2023-08-22 17:05:58 -0400

    Reformats board instance creation to use 'before'
    This is so that the board instance is recreated before every test.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,30 +1,30 @@
 require_relative '../lib/board.rb'
 
 RSpec.describe Board do
-  board_class = described_class.new 
+  before { @board_class = described_class.new }
   
   describe '#full?' do
     context 'board is empty' do
       it 'shows that board is not full' do
-        expect(board_class.full?).to be(false)
+        expect(@board_class.full?).to be(false)
       end
     end
 
     context 'board is partially filled' do
       it 'shows that board is not full' do
-        board_class.board = [["X","O","X"],
+        @board_class.board = [["X","O","X"],
         ["O"," "," "],
         [" "," "," "]]
-        expect(board_class.full?).to be(false)
+        expect(@board_class.full?).to be(false)
       end
     end
 
     context 'board is full' do
       it 'shows that board is full' do
-        board_class.board = [["X","O","X"],
+        @board_class.board = [["X","O","X"],
         ["O","X","O"],
         ["X","O","X"]]
-        expect(board_class.full?).to be(true)
+        expect(@board_class.full?).to be(true)
       end
     end
   end
