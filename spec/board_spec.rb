ee57ef48429de23772e616b59c0bc15f9426a6fa
murgraydj <murgraydj@gmail.com>
2023-08-22 16:50:58 -0400

    Adds board_spec.rb with testing for full? method

---
+++ spec/board_spec.rb
@@ -0,0 +1,35 @@
+require_relative '../lib/board.rb'
+
+RSpec.describe Board do
+
+  describe '#full?' do
+    context 'board is empty' do
+      it 'shows that board is not full' do
+        board_class = described_class.new 
+        expect(board_class.full?).to be(false)
+      end
+    end
+
+    context 'board is partially filled' do
+      it 'shows that board is not full' do
+        board_class = described_class.new 
+        board_class.board = [["X","O","X"],
+        ["O"," "," "],
+        [" "," "," "]]
+        expect(board_class.full?).to be(false)
+      end
+    end
+
+    context 'board is full' do
+      it 'shows that board is full' do
+        board_class = described_class.new 
+        board_class.board = [["X","O","X"],
+        ["O","X","O"],
+        ["X","O","X"]]
+        expect(board_class.full?).to be(true)
+      end
+    end
+  end
+
+
+end
