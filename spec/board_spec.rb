f2068339dc412f07417bbb638cfc0e8a7077b064
murgraydj <murgraydj@gmail.com>
2023-08-22 16:57:18 -0400

    Creates board instance at top of files
    The board instances were created at the top of each it statement before.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,18 +1,17 @@
 require_relative '../lib/board.rb'
 
 RSpec.describe Board do
-
+  board_class = described_class.new 
+  
   describe '#full?' do
     context 'board is empty' do
       it 'shows that board is not full' do
-        board_class = described_class.new 
         expect(board_class.full?).to be(false)
       end
     end
 
     context 'board is partially filled' do
       it 'shows that board is not full' do
-        board_class = described_class.new 
         board_class.board = [["X","O","X"],
         ["O"," "," "],
         [" "," "," "]]
@@ -22,7 +21,6 @@ RSpec.describe Board do
 
     context 'board is full' do
       it 'shows that board is full' do
-        board_class = described_class.new 
         board_class.board = [["X","O","X"],
         ["O","X","O"],
         ["X","O","X"]]
@@ -30,6 +28,5 @@ RSpec.describe Board do
       end
     end
   end
-
 
 end
